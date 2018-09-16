e00ef330e585fd9ef15139987886d7424c7f74ac
AaronCur <C00202505@itcarlow.ie>
2018-09-16 18:03:07 +0100

    Added autoscroll on new message if you are close to the bottom of the chat

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,7 +1,23 @@
 import React from "react";
+import ReactDOM from "react-dom"
 import Message from './Message'
 
 class MessageList extends React.Component {
+
+//Autoscroll to bottom if you are close to bottom of the screen
+//If you are far up the chat (catching up on messages) you wont be scrolled down
+componentWillUpdate(){
+  const node = ReactDOM.findDOMNode(this)
+  this.shouldScrollToBottom = node.scrollTop + node.clientHeight + 100 >+ node.scrollHeight
+}
+componentDidUpdate(){
+  if(this.shouldScrollToBottom)  {
+    const node = ReactDOM.findDOMNode(this)
+    node.scrollTop = node.scrollHeight
+  }
+
+}
+
   render() {
     return (
       <div className = "message-list">
